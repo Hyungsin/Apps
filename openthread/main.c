eb820e3b62a1b45579e8ce2e03d9cb9017b9b201
Dylan Dreyer <dyldreyer04@gmail.com>
2017-12-01 22:11:33 -0800

    adding in control message debug

--- openthread/main.c
+++ openthread/main.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <rtt_stdio.h>
 #include "ot.h"
+#include "debug.h"
 #include <openthread/udp.h>
 #include <openthread/cli.h>
 #include <openthread/thread.h>
@@ -33,7 +34,6 @@
 #define SAMPLE_JITTER   (15000000UL)
 
 #define ENABLE_DEBUG (1)
-#include "debug.h"
 uint32_t interval_with_jitter(void)
 {
     int32_t t = SAMPLE_INTERVAL;
@@ -41,7 +41,17 @@ uint32_t interval_with_jitter(void)
     t -= (SAMPLE_JITTER >> 1);
     return (uint32_t)t;
 }
-uint32_t rtchangeCt = 0;
+uint16_t nexthopChangeCt = 0;
+uint16_t addressMsgCnt = 0;
+uint8_t parentRequestMsgCnt = 0;
+uint8_t childIDRequestCnt = 0;
+uint8_t dataRequestCnt = 0;
+uint8_t childUpdateReqCnt = 0;
+uint8_t childUpdateRespCnt = 0;
+uint8_t announceCnt = 0;
+uint8_t orphanCnt = 0;
+uint8_t discoveryCnt = 0;
+
 int main(void)
 {
 #if DMAC_ENABLE
@@ -77,9 +87,10 @@ int main(void)
     buf[19] = 0;
     buf[18] = 0;
 
-    // Routing table changed on packet send.
+    // next hop change 
     buf[17] = 0;    
     buf[16] = 0;
+    // address msg counter
     buf[15] = 0;
     buf[14] = 0;
 
@@ -87,7 +98,26 @@ int main(void)
     buf[13] = 0;
     // link quality out
     buf[12] = 0;
-
+    // link quality in
+    buf[11] = 0;
+    // path cost
+    buf[10] = 0;
+    // parent request message count
+    buf[9] = 0;
+    // child ID request message count
+    buf[8] = 0;
+    // data request count
+    buf[7] = 0;
+    // child update request count
+    buf[6] = 0;
+    // child update response count
+    buf[5] = 0;
+    // announce count
+    buf[4] = 0;
+    // orphan count
+    buf[3] = 0;
+    // discovery count
+    buf[2] = 0;
 	while (1) {
 		//Sample
 	    //sample(&frontbuf);
@@ -108,20 +138,48 @@ int main(void)
             buf[18]++;
         }       
 
-        // Update routing table change number
-        buf[14] = rtchangeCt & 0xff;
-        buf[15] = (rtchangeCt >> 8) & 0xff;
-        buf[16] = (rtchangeCt >> 16) & 0xff;
-        buf[17] = (rtchangeCt >> 24) & 0xff;
+        // next hop change number
+        buf[16] = nexthopChangeCt & 0xff;
+        buf[17] = (nexthopChangeCt >> 8) & 0xff;
 
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%lu\n",rtchangeCt);
+        // address msg counter
+        buf[14] = addressMsgCnt & 0xff;
+        buf[15] = (addressMsgCnt >> 8) & 0xff;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA NEXT HOP CHANGE COUNT: %u\n",nexthopChangeCt);
+        printf("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB ADDRESS QUERY COUNT: %u\n", addressMsgCnt);
+
 
         // Link quality and next hop
         otRouterInfo routerInfo;
         otThreadGetParentInfo(sInstance, &routerInfo);
-        buf[12] = routerInfo.mLinkQualityOut
-        buf[13] = routerInfo.mNextHop
+        buf[10] = routerInfo.mPathCost;
+        buf[11] = routerInfo.mLinkQualityIn;
+        buf[12] = routerInfo.mLinkQualityOut;
+        buf[13] = routerInfo.mNextHop;
         
+        printf("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC PATH COST: %hu\n", routerInfo.mPathCost);
+        printf("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD LQ IN: %hu\n", routerInfo.mLinkQualityIn);
+        printf("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE LQ OUT: %hu\n", routerInfo.mLinkQualityOut);
+        printf("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF NEXT HOP: %hu\n", routerInfo.mNextHop);
+    
+        buf[9] = parentRequestMsgCnt;
+        printf("GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG PARENT REQ MSG CNT: %hu\n", parentRequestMsgCnt);
+        buf[8] = childIDRequestCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD ID REQ CNT: %hu\n", childIDRequestCnt);
+        buf[7] = dataRequestCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DATA REQ CNT: %hu\n", dataRequestCnt);
+        buf[6] = childUpdateReqCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD UPDATE REQ CNT: %hu\n", childUpdateRespCnt);
+        buf[5] = childUpdateRespCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD UPDATE RESP CNT: %hu\n", childUpdateRespCnt);
+        buf[4] = announceCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ANNOUNCE CNT: %hu\n", announceCnt);
+        buf[3] = orphanCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ORPHAN CNT: %hu\n", orphanCnt);
+        buf[2] = discoveryCnt;
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DISCOVERY CNT: %hu\n", discoveryCnt);
+
+
         // Source addr setting
         uint8_t source = OPENTHREAD_SOURCE;
         buf[0] = source;
