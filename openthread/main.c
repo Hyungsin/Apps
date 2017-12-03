f8a5557fa7cb19edf1b4d46708a53a081f2e9710
Dylan Dreyer <dyldreyer04@gmail.com>
2017-12-02 23:59:32 -0800

    adding revised control data

--- openthread/main.c
+++ openthread/main.c
@@ -41,16 +41,12 @@ uint32_t interval_with_jitter(void)
     t -= (SAMPLE_JITTER >> 1);
     return (uint32_t)t;
 }
-uint16_t nexthopChangeCt = 0;
-uint16_t addressMsgCnt = 0;
-uint8_t parentRequestMsgCnt = 0;
-uint8_t childIDRequestCnt = 0;
-uint8_t dataRequestCnt = 0;
-uint8_t childUpdateReqCnt = 0;
-uint8_t childUpdateRespCnt = 0;
-uint8_t announceCnt = 0;
-uint8_t orphanCnt = 0;
-uint8_t discoveryCnt = 0;
+uint8_t borderRouteChangeCnt = 0;
+uint32_t addressMsgCnt = 0;
+uint32_t joiningMsgCnt = 0;
+uint32_t routingMsgCnt = 0;
+uint32_t linkMsgCnt = 0;
+uint32_t controlMsgCnt = 0;
 
 int main(void)
 {
@@ -80,45 +76,16 @@ int main(void)
 	otIp6AddressFromString("fdde:ad00:beef:0000:c684:4ab6:ac8f:9fe5", &messageInfo.mPeerAddr);
     messageInfo.mPeerPort = 1234;
     messageInfo.mInterfaceId = 1;
-    char buf[20];
-    for (int i =0; i<20; i++) {
+    char buf[30];
+    for (int i =0; i<30; i++) {
         buf[i] = 0xff;
     }
-    buf[19] = 0;
-    buf[18] = 0;
 
-    // next hop change 
-    buf[17] = 0;    
-    buf[16] = 0;
-    // address msg counter
-    buf[15] = 0;
-    buf[14] = 0;
-
-    // next hop
-    buf[13] = 0;
-    // link quality out
-    buf[12] = 0;
-    // link quality in
-    buf[11] = 0;
-    // path cost
-    buf[10] = 0;
-    // parent request message count
-    buf[9] = 0;
-    // child ID request message count
-    buf[8] = 0;
-    // data request count
-    buf[7] = 0;
-    // child update request count
-    buf[6] = 0;
-    // child update response count
-    buf[5] = 0;
-    // announce count
-    buf[4] = 0;
-    // orphan count
-    buf[3] = 0;
-    // discovery count
-    buf[2] = 0;
-	while (1) {
+    for (int i = 0; i < 28; i++) {
+        buf[i] = 0x0;
+    }
+	
+    while (1) {
 		//Sample
 	    //sample(&frontbuf);
 		//aes_populate();
@@ -131,63 +98,71 @@ int main(void)
         if (message == NULL) {
             printf("error in new message");
         }
-
+        
+        
         // Tx Sequence number setting
         buf[19]++;
         if (buf[19] == 0) {
             buf[18]++;
         }       
 
-        // next hop change number
-        buf[16] = nexthopChangeCt & 0xff;
-        buf[17] = (nexthopChangeCt >> 8) & 0xff;
-
-        // address msg counter
+        // Address message counts
         buf[14] = addressMsgCnt & 0xff;
         buf[15] = (addressMsgCnt >> 8) & 0xff;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA NEXT HOP CHANGE COUNT: %u\n",nexthopChangeCt);
-        printf("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB ADDRESS QUERY COUNT: %u\n", addressMsgCnt);
+        buf[16] = (addressMsgCnt >> 16) & 0xff;
+        buf[17] = (addressMsgCnt >> 24) & 0xff;
 
+        // next hop change number
+        buf[13] = borderRouteChangeCnt;
 
-        // Link quality and next hop
+        // Link quality, next hop, path cost
         otRouterInfo routerInfo;
         otThreadGetParentInfo(sInstance, &routerInfo);
-        buf[10] = routerInfo.mPathCost;
-        buf[11] = routerInfo.mLinkQualityIn;
-        buf[12] = routerInfo.mLinkQualityOut;
-        buf[13] = routerInfo.mNextHop;
-        
+        buf[9] = routerInfo.mPathCost;
+        buf[10] = routerInfo.mLinkQualityIn;
+        buf[11] = routerInfo.mLinkQualityOut;
+        buf[12] = routerInfo.mNextHop;
+
+        buf[5] = joiningMsgCnt & 0xff;
+        buf[6] = (joiningMsgCnt >> 8) & 0xff; 
+        buf[7] = (joiningMsgCnt >> 16) & 0xff; 
+        buf[8] = (joiningMsgCnt >> 24) & 0xff; 
+
+        buf[1] = routingMsgCnt & 0xff;
+        buf[2] = (routingMsgCnt >> 8) & 0xff; 
+        buf[3] = (routingMsgCnt >> 16) & 0xff; 
+        buf[4] = (routingMsgCnt >> 24) & 0xff;
+
+        buf[20] = linkMsgCnt & 0xff;
+        buf[21] = (linkMsgCnt >> 8) & 0xff; 
+        buf[22] = (linkMsgCnt >> 16) & 0xff; 
+        buf[23] = (linkMsgCnt >> 24) & 0xff;
+
+        buf[24] = controlMsgCnt & 0xff;
+        buf[25] = (controlMsgCnt >> 8) & 0xff; 
+        buf[26] = (controlMsgCnt >> 16) & 0xff; 
+        buf[27] = (controlMsgCnt >> 24) & 0xff;
+
+
         printf("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC PATH COST: %hu\n", routerInfo.mPathCost);
         printf("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD LQ IN: %hu\n", routerInfo.mLinkQualityIn);
         printf("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE LQ OUT: %hu\n", routerInfo.mLinkQualityOut);
         printf("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF NEXT HOP: %hu\n", routerInfo.mNextHop);
-    
-        buf[9] = parentRequestMsgCnt;
-        printf("GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG PARENT REQ MSG CNT: %hu\n", parentRequestMsgCnt);
-        buf[8] = childIDRequestCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD ID REQ CNT: %hu\n", childIDRequestCnt);
-        buf[7] = dataRequestCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DATA REQ CNT: %hu\n", dataRequestCnt);
-        buf[6] = childUpdateReqCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD UPDATE REQ CNT: %hu\n", childUpdateRespCnt);
-        buf[5] = childUpdateRespCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CHILD UPDATE RESP CNT: %hu\n", childUpdateRespCnt);
-        buf[4] = announceCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ANNOUNCE CNT: %hu\n", announceCnt);
-        buf[3] = orphanCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ORPHAN CNT: %hu\n", orphanCnt);
-        buf[2] = discoveryCnt;
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DISCOVERY CNT: %hu\n", discoveryCnt);
-
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA BORDER ROUTER NEXT HOP CHANGE CNT: %hu\n", borderRouteChangeCnt);
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ADDRESS MSG CNT: %lu\n", addressMsgCnt);
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA JOINING MSG CNT: %lu\n", joiningMsgCnt);
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ROUTING MSG CNT: %lu\n", routingMsgCnt);
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LINK MSG CNT: %lu\n", linkMsgCnt);
+        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CONTROl MSG CNT: %lu\n", controlMsgCnt);
 
         // Source addr setting
         uint8_t source = OPENTHREAD_SOURCE;
         buf[0] = source;
-        error = otMessageSetLength(message, 20);
+        error = otMessageSetLength(message, 30);
         if (error != OT_ERROR_NONE) {
             printf("error in set length\n");
         }
-        otMessageWrite(message, 0, buf, 20);
+        otMessageWrite(message, 0, buf, 30);
 		
         DEBUG("[Main] Tx UDP packet\n");
         error = otUdpSend(&mSocket, message, &messageInfo);
